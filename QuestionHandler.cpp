dcaa4cc987109161870fad5aa6121e812c0d1381
Yumi Ta <itsyumietanaka@gmail.com>
2018-09-23 17:11:21 -0400

    Fix bugs with question response.

--- QuestionHandler.cpp
+++ QuestionHandler.cpp
@@ -1,5 +1,6 @@
 #include "QuestionHandler.hpp"
 #include <iostream>
+#include <string>
 
 const char* QuestionHandler::correct_answer_message = "Correct!";
 const char* QuestionHandler::incorrect_answer_message = "Incorrect!";
@@ -9,8 +10,19 @@ bool QuestionHandler::ask_question(Question &question){
    for (auto i = question.answers.begin(); i != question.answers.end(); i++){
       std::cout << i - question.answers.begin() + 1 << " - " << *i << std::endl;
    }
+
    unsigned int answer;
-   std::cin >> answer;
+   input:
+   try{
+      std::string temp;
+      std::getline(std::cin, temp); 
+      answer = std::stoi(temp);
+   }
+   catch(...){
+      answer = UINT32_MAX;
+      std::cout << "You must enter a numeric value." << std::endl;
+      goto input;
+   }
 
    if (answer == question.correct_answer + 1){
       std::cout << correct_answer_message << std::endl;
@@ -26,8 +38,19 @@ void QuestionHandler::ask_question(QuestionResult &question){
    for (auto i = question.question.answers.begin(); i != question.question.answers.end(); i++){
       std::cout << i - question.question.answers.begin() + 1 << " - " << *i << std::endl;
    }
+
    unsigned int answer;
-   std::cin >> answer;
+   input:
+   try{
+      std::string temp;
+      std::getline(std::cin, temp); 
+      answer = std::stoi(temp);
+   }
+   catch(...){
+      answer = UINT32_MAX;
+      std::cout << "You must enter a numeric value." << std::endl;
+      goto input;
+   }
 
    if (answer == question.question.correct_answer + 1){
       std::cout << correct_answer_message << std::endl;
