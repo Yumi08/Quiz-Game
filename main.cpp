83e1b24d5f113255b6003d9ace49562f8d5cd1ed
Yumi Ta <itsyumietanaka@gmail.com>
2018-09-23 19:46:47 -0400

    Work on json importing.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,32 @@
 #include <vector>
 #include "QuestionResult.hpp"
 #include "QuestionGroup.hpp"
+#include <json/reader.h>
+#include <fstream>
+#include <string>
+#include <streambuf>
 
 void setup(){
    srand(time(0));
 }
 
+void import_questions(){
+   std::ifstream questions_file("questions.json", std::ios::in | std::ios::ate);
+   std::string file_text;
+
+   questions_file.seekg(0, std::ios::end);
+   file_text.reserve(questions_file.tellg());
+   questions_file.seekg(0, std::ios::beg);
+
+   file_text.assign(std::istreambuf_iterator<char>(questions_file),
+   std::istreambuf_iterator<char>());
+
+   
+}
+
 int main(){
    setup();
+   import_questions();
 
    QuestionGroup questions;
    questions.push_back({
