1ed9a22f823610226a605cf1166c0a0a2832e2a6
Yumi Ta <itsyumietanaka@gmail.com>
2018-09-23 17:29:35 -0400

    QuestionGroup now shows quiz results.

--- QuestionGroup.cpp
+++ QuestionGroup.cpp
@@ -1,7 +1,18 @@
 #include "QuestionGroup.hpp"
 #include "QuestionHandler.hpp"
+#include <iostream>
 
 void QuestionGroup::ask_all_questions(){
    for (auto i = begin(); i != end(); i++)
-      QuestionHandler::ask_question(*i);
+   QuestionHandler::ask_question(*i);
+
+   unsigned short percentage;
+   unsigned int correct = 0;
+
+   for (auto i = begin(); i != end(); i++)
+   correct += (*i).correct;
+
+   percentage = 100 * correct / size();
+
+   std::cout << "You got " << correct << "/" << size() << "(" << percentage << "%)" << " questions correct!" << std::endl;
 }
